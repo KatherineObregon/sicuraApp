a8f76a787502e2edd33a13ba49ac3641062a91f2
KatherineObregon <a20181823@pucp.edu.pe>
2023-12-05 18:15:02 -0500

    creacion de alertas

---
+++ app/src/main/java/com/example/sicuraapp/Entities/Alerta.java
@@ -0,0 +1,43 @@
+package com.example.sicuraapp.Entities;
+
+import java.io.Serializable;
+
+public class Alerta implements Serializable {
+
+    private String id;
+    private String hora;
+    private String fecha;
+    private String idUsuario;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
--- app/src/main/java/com/example/sicuraapp/Home.java
+++ app/src/main/java/com/example/sicuraapp/Home.java
@@ -1,34 +1,89 @@
 package com.example.sicuraapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.sicuraapp.Entities.Alerta;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class Home extends AppCompatActivity {
 
     Button logout;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
-    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    String idUsuarioActual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.home);
         super.onCreate(savedInstanceState);
-        mAuth= FirebaseAuth.getInstance();
-        db= FirebaseFirestore.getInstance();
 
 
 
         setContentView(R.layout.activity_home);
         logout = findViewById(R.id.btn_logout);
+
+
+        mAuth= FirebaseAuth.getInstance();
+        db= FirebaseFirestore.getInstance();
+
+
+    }
+
+    public void botonAlerta(View view){
+        idUsuarioActual= mAuth.getCurrentUser().getUid();
+
+        // Obtiene la fecha y hora actual
+        Date currentDate = new Date();
+
+        // Define el formato deseado para la fecha y hora, incluyendo milisegundos
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.getDefault());
+
+        // Formatea la fecha y hora actual como un String separado por un guion
+        String formattedDateTime = dateFormat.format(currentDate);
+        String[] dateTimeParts = formattedDateTime.split(" ");
+
+        // Elimina los espacios en blanco y concatena las partes con un guion
+        String fechaHora= dateTimeParts[0].replace("-", "") + "-" + dateTimeParts[1].replace(":", "");
+
+        String idAlerta = idUsuarioActual+fechaHora;
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", idAlerta);
+        map.put("fecha", dateTimeParts[0]);
+        map.put("hora", dateTimeParts[1]);
+        map.put("idUsuario", idUsuarioActual);
+
+        db.collection("alerta").document(idAlerta).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+//                finish();
+//                startActivity(new Intent(Home.this, Home.class));
+                Toast.makeText(Home.this, "Alerta creada", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Home.this, "Alerta no creada :(", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     public void irAMasOpciones (View view){
