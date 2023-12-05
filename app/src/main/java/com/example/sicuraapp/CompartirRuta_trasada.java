efb8720e770d8bd72f7873492f76527b42a83c8b
KatherineObregon <a20181823@pucp.edu.pe>
2023-12-04 20:53:37 -0500

    avance maps modo 2

--- app/src/main/java/com/example/sicuraapp/CompartirRuta_trasada.java
+++ app/src/main/java/com/example/sicuraapp/CompartirRuta_trasada.java
@@ -6,21 +6,31 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class CompartirRuta_trasada extends AppCompatActivity implements OnMapReadyCallback {
 
     FusedLocationProviderClient fusedLocationProviderClient;
     GoogleMap map;
+
+    double userLat, userLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +54,35 @@ public class CompartirRuta_trasada extends AppCompatActivity implements OnMapRea
 
         map.setMyLocationEnabled(true);
         map.getUiSettings().setZoomControlsEnabled(true);
+
+        fetchMyLocation();
+
+    }
+
+    private void fetchMyLocation() {
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                userLat= location.getLatitude();
+                userLong = location.getLongitude();
+
+                LatLng latLng = new LatLng(userLat, userLong);
+
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng)
+                        .zoom(12).build();
+
+                map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                map.addMarker(new MarkerOptions().position(latLng).title("Posición actual"));
+
+            }
+        });
+
     }
 }
